4a0d3cf49e36fd8113d2a47d76db2630e131cb6c
Seungwon-Choi <bestwondev@gmail.com>
2025-12-07 18:39:47 +0900

    initial commit

---
+++ src/main/java/com/communityfeedservice/CommunityFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.communityfeedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityFeedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityFeedServiceApplication.class, args);
+    }
+
+}
